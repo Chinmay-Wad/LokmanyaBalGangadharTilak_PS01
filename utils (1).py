6d2516c55696096468055d4f2cac33abd305eac7
Chinmay-Wad <vihanwad@gmail.com>
2026-02-01 16:21:13 +0530

    Add files via upload

---
+++ utils (1).py
@@ -0,0 +1,15 @@
+ABBREVIATIONS = {
+    "BD": "twice daily",
+    "OD": "once daily",
+    "TID": "three times daily",
+    "IV": "intravenous",
+    "PO": "oral",
+    "SOS": "as needed",
+    "x": ""
+}
+
+FORMS = {
+    "Tab": "tablet",
+    "Inj": "injection",
+    "Cap": "capsule"
+}
